4d423f9b77a41388c66de13b6f6725cb76567cbc
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2024-07-10 15:50:01 +0300

    Initial project skeletton

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <article className='screen'>
+      {/* <!--Time And Battery--> */}
+        <section className="time">
+            <p className="clock"><span>15:08</span></p>
+            <p className="battery"><i className="fa-solid fa-battery-empty"></i></p>
+        </section> 
+
+        {/* <!--Nav--> */}
+        <section className="nav">
+            <p className="arrows"><i className="fa-solid fa-down-left-and-up-right-to-center"></i></p> 
+
+            <ul className="nav-links">
+                <li><a href="#">Калкулатор</a></li>
+                <li><a href="#">Конвертор</a></li>
+            </ul> 
+
+            <p className="dots">&#8942;</p>
+        </section> 
+
+        {/* <!--Section Container--> */}
+        <div className="section-container">
+            <section className="output">
+                <input type="text" className="initial-input" placeholder="0" />
+                <input type="text" className="result" placeholder="=" />
+            </section>
+
+            {/* <!--Section Buttons--> */}
+            <section className="buttons-container">
+                <button className="btn orange-text">AC</button>
+                <button className="btn orange-text"><i className="fa-solid fa-delete-left"></i></button>
+                <button className="btn orange-text">%</button>
+                <button className="btn orange-text">/</button>
+    
+                <button className="btn">7</button>
+                <button className="btn">8</button>
+                <button className="btn">9</button>
+                <button className="btn orange-text">x</button>
+    
+                <button className="btn">4</button>
+                <button className="btn">5</button>
+                <button className="btn">6</button>
+                <button className="btn orange-text">+</button>
+    
+                <button className="btn">1</button>
+                <button className="btn">2</button>
+                <button className="btn">3</button>
+                <button className="btn orange-text">-</button>
+    
+                <button className="btn orange-text"><i className="fa-solid fa-arrows-rotate"></i></button>
+                <button className="btn">0</button>
+                <button className="btn">,</button>
+                <button className="btn btn-orange-fill">=</button>
+            </section>
+        </div>
+        
+        {/* <!--Menu buttons--> */}
+        <section className="menu-buttons">
+            <p><i className="fa-solid fa-square"></i></p>
+            <p><i className="fa-regular fa-circle-dot"></i></p>
+            <p><i className="fa-solid fa-caret-left"></i></p>
+        </section>
+    </article>
   )
 }
 
